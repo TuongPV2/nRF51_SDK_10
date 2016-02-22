b7a5c5a7cc50d092a399d620722580bdd3ca6f2d
TuongPV <20093217@student.hut.edu.vn>
2016-02-22 17:39:15 +0700

    update bsp/CAP1114.h

--- examples/bsp/CAP1114.h
+++ examples/bsp/CAP1114.h
@@ -30,20 +30,20 @@ SOFTWARE.
  *  01 Feb, 2016
  *	V1.0
  */
- ##ifndef CAP1114_H
+ #ifndef CAP1114_H
  #define CAP1114_H
 
  #define CAP1114_CS			0x00	/** Main Status Control */
  #define BUTTON_S_1			0x03	/** Button status 1	*/
  #define BUTTON_S_2			0x04	/** Button status 2	*/
  #define BUILD_REV			0x05	/** Build revision */
- #define SLIDER_POS 		0x06	/** Slider Position or Volumetric Data */
+ //#define SLIDER_POS 		0x06	/** Slider Position or Volumetric Data */
  #define CAP1114_ID			0x08	/** CAP1114 vendor ID */
- #define VOL_STEP			0x09	/** Volumetric Step */
- #define NOISE_S_1			0x0A	/** Noise status 1*/
- #define NOISE_S_2			0x0B	/** Noise status 2*/
- #define LID_CLOSURE_S_1	0x0C	/** Lid Closure Status 1 */
- #define LID_CLOSURE_S_2	0x0D	/** Lid Closure Status 2 */
+ //#define VOL_STEP			0x09	/** Volumetric Step */
+ //#define NOISE_S_1			0x0A	/** Noise status 1*/
+ //#define NOISE_S_2			0x0B	/** Noise status 2*/
+ // #define LID_CLOSURE_S_1	0x0C	/** Lid Closure Status 1 */
+ // #define LID_CLOSURE_S_2	0x0D	/** Lid Closure Status 2 */
  #define GPIO_STATUS		0x0E	/** GPIO Status */
  #define GROUP_STATUS		0x0F	/** Group Status */
  #define SENSOR_1_DELC		0x10	/** Stores the delta count for CS1 */
@@ -71,7 +71,60 @@ SOFTWARE.
  #define CALIB_ACTIVATE		0x26	/** Calibration Activate */
  #define INT_EN_1			0x27	/** Interrupt Enable 1 */
  #define INT_EN_2			0x28	/** Interrupt Enable 2 */
+ #define SLEEP_CHN_CTRL		0x29	/** Sleep Channel Control */
+ #define MULTI_PRESS_CFG	0x2A	/** Multi press configuration */
+ #define LID_CLOSURE_CFG	0x2B	/** Controls Lid Closure detection and operation */
+ #define LID_CLOSURE_CTRL	0x2C	/** Lid Closure Queue ctrl */
+ #define LID_CLOSURE_PAT_1	0x2D	/** Lid Closure Pattern 1 */
+ #define LID_CLOSURE_PAT_2	0x2E	/** Lid Closure Pattern 2 */
+ #define RECALIBRATION_CFG	0x2F	/** Recalibration configuration */
+ #define SENSOR_1_THRESH	0x30 	/** Sensor  1 Threshold */
+ #define SENSOR_2_THRESH	0x31 	/** Sensor  2 Threshold */
+ #define SENSOR_3_THRESH	0x32 	/** Sensor  3 Threshold */
+ #define SENSOR_4_THRESH	0x33 	/** Sensor  4 Threshold */
+ #define SENSOR_5_THRESH	0x34 	/** Sensor  5 Threshold */
+ #define SENSOR_6_THRESH	0x35 	/** Sensor  6 Threshold */
+ #define SENSOR_7_THRESH	0x36 	/** Sensor  7 Threshold */
+ #define GROUP_THRES		0x37 	/** Group Threshold */
+
+ #define LED_GPIO_DIR		0x70	/** LED / GPIO Direction */
+ #define LED_GPIO_TYPE		0x71 	/** LED / GPIO Output */
+ #define GPIO_INPUT			0x72 	/** GPIO Input */
+ #define LED_OUT_CTRL_1		0x73 	/** LED Output Control 1 */
+ #define LED_OUT_CTRL_2		0x74 	/** LED Output Control 2 */
+ #define LED_POL_1			0x75 	/** LED Polarity 1 */
+ #define LED_POL_2			0x76 	/** LED Polarity 2 */
+
+ #define PROD_ID 			0xFD 	/** Product ID */
+ #define MANUF_ID			0xFE	/** Manufacturer ID */
+ #define REV_ID				0xFF 	/** Revision */
+
  
+
+
+
+// enum {
+// 	CAP1106,
+// 	CAP1114,
+// 	CAP1126,
+// 	CAP1188,
+// };
+
+
+// struct cap11xx_hw_model {
+// 	uint8_t product_id;
+// 	uint32_t num_channels;
+// };
+
+// static const struct cap11xx_hw_model cap11xx_devices[] = {
+// 	[CAP1106] = { .product_id = 0x55, .num_channels = 6 },
+// 	[CAP1114] = { .product_id = 0x3a, .num_channels = 14 },	
+// 	[CAP1126] = { .product_id = 0x53, .num_channels = 6 },
+// 	[CAP1188] = { .product_id = 0x50, .num_channels = 8 },
+// };
+
+
+
 
 
  #endif	/* CAP1114_H */
